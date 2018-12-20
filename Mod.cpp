1a1de75fb279c705a894ef4b63a32d23e98bb1e3
givenameplz <givenameplz@googlemail.com>
2018-12-20 22:25:39 +0100

    Create Mod.cpp
    Just adding a basic Mod.cpp.

---
+++ Mod.cpp
@@ -0,0 +1,22 @@
+name = "Manticore CorePack";
+author = "Mattress";
+description = "Manticore CorePack";
+overview = "CorePack for Insignia/Badges and miscellaneous of Task Force Manticore."; 
+overviewPicture = "TFM_Logo_1024x1024.paa";
+picture = "TFM_Logo_1024x1024.paa"; 
+logo = "TFM_Logo_128x128.paa";
+logoOver = "TFM_Logo_128x128.paa";
+logoSmall = "TFM_Logo_64x64.paa";
+action = "http://taskforcemanticore.enjin.com/";
+actionName = "Unit Website";
+tooltip = "Manticore CorePack";
+tooltipOwned = "Manticore CorePack (Owned)";
+dlcColor[] = 
+{
+	0.11,
+	0.13,
+	0.32,
+	1
+};
+hideName = 0;
+hidePicture = 0;
